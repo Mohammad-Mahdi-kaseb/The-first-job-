cb26f8d80fbd3a56854798bd276cb811f3d10410
Mohammad-Mahdi-kaseb <mahdi15.20781338@gmail.com>
2020-08-31 17:30:31 +0430

    update

--- 6mmaster.py
+++ 6mmaster.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+def callback(arg1 , arg2 ,arg3):
+    p1.set(e1.get())
 def b1():
     pass
 
@@ -45,27 +47,49 @@ tk.Button(timer , text = "SET" , command = b1 ).grid(row = 2 , column = 2)
 tk.Button(timer , text = "CANCEL" , command = root.destroy  ).grid(row = 3 , column = 0 , columnspan = 3
  , sticky = tk.W+tk.E )
 ###########################################
+patient1 = tk.LabelFrame(patient , text='Patient1')
+patient1.grid(row = 0 , column = 0 , padx=10)
 
+tk.Label(patient1, text = "NAME:").grid(row=0, column=0)
+tk.Label(patient1, text = "TIME:").grid(row=1, column=0)
+patient2 = tk.LabelFrame(patient , text='Patient2')
+patient2.grid(row = 1 , column = 0 , padx=10)
 
-tk.Label(patient, text = "1_NAME:").grid(row=0, column=0)
-tk.Label(patient, text = "1_TIME:").grid(row=1, column=0)
-tk.Label(patient, text = "2_NAME:").grid(row=2, column=0)
-tk.Label(patient, text = "2_TIME:").grid(row=3, column=0)
-tk.Label(patient, text = "3_NAME:").grid(row=4, column=0)
-tk.Label(patient, text = "3_TIME:").grid(row=5, column=0)
+tk.Label(patient2, text = "NAME:").grid(row=0, column=0)
+tk.Label(patient2, text = "TIME:").grid(row=1, column=0)
+patient3 = tk.LabelFrame(patient , text='Patient3')
+patient3.grid(row = 2 , column = 0 , padx=10)
+
+tk.Label(patient3, text = "NAME:").grid(row=0, column=0)
+tk.Label(patient3, text = "TIME:").grid(row=1, column=0)
+
 ############################################
+h_p_1 = tk.StringVar()
+h_p_1.set("12")
+m_p_1 = tk.StringVar()
+m_p_1.set("59")
+s_p_1 = tk.StringVar()
+s_p_1.set("59")
+f1 = tk.Frame(patient1)
+f1.grid(row= 1 , column = 1)
+tk.Spinbox(f1 ,
+ from_ = 0 ,
+  to = 23 ,
+   wrap = True ,
+    textvariable = h_p_1 ,
+     width = 2 , 
+     state = "readonly").grid(row = 0 , column = 0)
 e1 = tk.StringVar()
+e1.trace("w" , callback)
 e2 = tk.StringVar()
+e2.trace("w" , callback)
 e3 = tk.StringVar()
-e4 = tk.StringVar()
-e5 = tk.StringVar()
-e6 = tk.StringVar()
-tk.Entry(patient ,textvariable = e1 ).grid(row = 0 , column = 1)
-tk.Entry(patient ,textvariable = e2).grid(row = 1 , column = 1)
-tk.Entry(patient ,textvariable = e3).grid(row = 2 , column = 1)
-tk.Entry(patient ,textvariable = e4 ).grid(row = 3 , column = 1)
-tk.Entry(patient ,textvariable = e5 ).grid(row = 4 , column = 1)
-tk.Entry(patient ,textvariable = e6 ).grid(row = 5 , column = 1)
+e3.trace("w" , callback)
 
+tk.Entry(patient1 ,textvariable = e1 ).grid(row = 0 , column = 1)
+
+tk.Entry(patient2 ,textvariable = e2 ).grid(row = 0 , column = 1)
+
+tk.Entry(patient3 ,textvariable = e3 ).grid(row = 0 , column = 1)
 
 root.mainloop()
